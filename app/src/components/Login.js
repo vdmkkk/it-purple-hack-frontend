513ae1fc3b3eaaac8c66f7a46dbd627d4086d2c5
AlexGorin <alexgorin37@gmail.com>
2024-03-09 23:47:04 +0300

    Logging page - desktop

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -3,6 +3,12 @@ import { useState } from 'react';
 import '../styles/Login.css';
 import { useNavigate } from 'react-router-dom';
 
+import line from "../assets/icons/Line.svg"
+import green_ball from "../assets/icons/green_ball.svg"
+import blue_ball from "../assets/icons/blue_ball.svg"
+import red_ball from "../assets/icons/red_ball.svg"
+import purple_ball from "../assets/icons/purple_ball.svg"
+
 function Login() {
 
     let navigate = useNavigate();
@@ -33,11 +39,26 @@ function Login() {
 
 
     return(
-        <div className='login'>
-            <input className='input' value={login} onChange={(e) => handleLogin(e.target.value)} />
-            <input className='input' value={password} onChange={(e) => handlePassword(e.target.value)} />
-            <h4 className='error'>{err}</h4>
-            <div onClick={(e) => handleLoginBtn(e)}><p>Войти</p></div>
+        <div className='back-ground'>
+            <div className='conteiner'>
+            <img src={blue_ball} className='blue_ball'></img>
+            <img src={green_ball} className='green_ball'></img>
+            <img src={red_ball} className='red_ball'></img>
+            <img src={purple_ball} className='purple_ball'></img>
+                <div className='inside-cont'>
+                    <div className='label'>
+                        <p>Вход в систему</p>
+                        <p>Админ-панели <span className='avito'>Avito</span></p>
+                    </div>
+                    <img src={line} className='line'></img>
+                    <div className='input-field'>
+                        <input className='input' value={login} placeholder="example@gmail.com" onChange={(e) => handleLogin(e.target.value)} />
+                        <input className='input' value={password} placeholder="Введите пароль" onChange={(e) => handlePassword(e.target.value)} />
+                        <div onClick={(e) => handleLoginBtn(e)} className='enter-button'><p>Войти</p></div>
+                        <h4 className='error'>{err}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
+
 // ? ROUTES HERE ? //
 import App from "./App";
 import Login from "./components/Login";
@@ -20,6 +21,12 @@ const router = createBrowserRouter([
     element: <Login/>
   }
 ]);
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap')
+</style>
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
