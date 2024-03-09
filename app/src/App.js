fbda6f40bf7887272b5285565647876c61448b6f
AlexGorin <alexgorin37@gmail.com>
2024-03-09 14:28:46 +0300

    added ChartJS Graphs

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Graph } from './components/Graph';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Graph></Graph>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
