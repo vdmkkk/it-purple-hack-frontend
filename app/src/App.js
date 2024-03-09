9066237c1b2ed0ba0da41600d2efd213f3098e44
vdmkkk <soloviov.vad@gmail.com>
2024-03-09 14:26:33 +0300

    login complete

--- app/src/App.js
+++ app/src/App.js
@@ -8,21 +8,11 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="login" element={<Login/>} />
-      </Routes>
-    </Router>
-  );
-}
-
-function Home() {
-  return(
     <div>
       <h1>HOME</h1>
     </div>
   );
 }
+
 
 export default App;
