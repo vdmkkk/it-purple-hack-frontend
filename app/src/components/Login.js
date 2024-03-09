5a12b1f7435e0abd7fda83e3eeded4cf47952808
vdmkkk <soloviov.vad@gmail.com>
2024-03-09 17:31:02 +0300

    added svg

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import '../styles/Login.css';
 import { useNavigate } from 'react-router-dom';
+import show from '../assets/icons/show.svg'
+import hide from '../assets/icons/hide.svg'
+
 
 function Login() {
 
@@ -10,6 +13,7 @@ function Login() {
     const [login, setLogin] = useState();
     const [password, setPassword] = useState();
     const [err, setErr] = useState("");
+    const [isShown, setIsShown] = useState(false);
 
     function handleLogin(str) {
         console.log(str);
@@ -34,8 +38,12 @@ function Login() {
 
     return(
         <div className='login'>
-            <input className='input' value={login} onChange={(e) => handleLogin(e.target.value)} />
             <input className='input' value={password} onChange={(e) => handlePassword(e.target.value)} />
+
+            <div className='input-wrapper'>
+                <input type={isShown ? 'text' : 'password'} className='input' value={login} onChange={(e) => handleLogin(e.target.value)} />
+                <img onClick={() => setIsShown(!isShown)} src={isShown ? hide : show}/>
+            </div>
             <h4 className='error'>{err}</h4>
             <div onClick={(e) => handleLoginBtn(e)}><p>Войти</p></div>
         </div>
