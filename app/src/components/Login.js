2b7b980aaaef306ad504f033cd7c25c3dc5eca4a
AlexGorin <alexgorin37@gmail.com>
2024-03-10 03:27:27 +0300

    Completed: Login page

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -9,13 +9,18 @@ import blue_ball from "../assets/icons/blue_ball.svg"
 import red_ball from "../assets/icons/red_ball.svg"
 import purple_ball from "../assets/icons/purple_ball.svg"
 
+import show from '../assets/icons/show.svg'
+import hide from '../assets/icons/hide.svg'
+
 function Login() {
 
     let navigate = useNavigate();
 
-    const [login, setLogin] = useState();
-    const [password, setPassword] = useState();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const [err, setErr] = useState("");
+    const [value, setValue] = useState('');
+    const [isShown, setIsShown] = useState(false);
 
     function handleLogin(str) {
         console.log(str);
@@ -31,31 +36,73 @@ function Login() {
         if (login == 'vdmk' && password == '123') {
             // TODO: store this into cookie?
             setErr('');
+            setLogin('');
+            setPassword('');
             navigate('/');
         } else {
-            setErr('Неправильный логин или пароль!');
+            if (login == '' || password == '') {
+                setErr('Пожалуйтса, заполните все поля');
+            } else {
+                setErr('Неправильный логин или пароль!');
+            }
         }
     }
+
+    function handleFocus() {
+        setErr(false); // Reset error state when input is focused
+    }
+
+    function handleBlur() {
+        if (value.trim() === '') {
+          setErr('Пожалуйтса, заполните все поля'); // Set error state if input is empty
+        }
+    }
+
+    function handleClickLogin(valueC) {
+        handleLogin(valueC)
+        
+        setValue(valueC)
+    }
+
+    function handleClickPassword (valueC) {
+            handlePassword(valueC)
+            setValue(valueC)
+    }
+
+    
+
+
 
 
     return(
         <div className='back-ground'>
-            <div className='conteiner'>
-            <img src={blue_ball} className='blue_ball'></img>
-            <img src={green_ball} className='green_ball'></img>
-            <img src={red_ball} className='red_ball'></img>
-            <img src={purple_ball} className='purple_ball'></img>
-                <div className='inside-cont'>
-                    <div className='label'>
-                        <p>Вход в систему</p>
-                        <p>Админ-панели <span className='avito'>Avito</span></p>
-                    </div>
-                    <img src={line} className='line'></img>
-                    <div className='input-field'>
-                        <input className='input' value={login} placeholder="example@gmail.com" onChange={(e) => handleLogin(e.target.value)} />
-                        <input className='input' value={password} placeholder="Введите пароль" onChange={(e) => handlePassword(e.target.value)} />
-                        <div onClick={(e) => handleLoginBtn(e)} className='enter-button'><p>Войти</p></div>
-                        <h4 className='error'>{err}</h4>
+            <div className='empty-div'>
+                <img src={blue_ball} className='blue_ball'></img>
+                <img src={green_ball} className='green_ball'></img>
+                <img src={red_ball} className='red_ball'></img>
+                <img src={purple_ball} className='purple_ball'></img>
+                <div className='conteiner'>
+                
+                    <div className='inside-cont'>
+                        <div className='label'>
+                            <p>Вход в систему</p>
+                            <p>Админ-панели <span className='avito'>Avito</span></p>
+                        </div>
+                        <img src={line} className='line'></img>
+                        <div className='input-field'>
+                            <input  className='input' value={login} placeholder="example@gmail.com" onFocus={handleFocus}
+        onBlur={handleBlur} onChange={(e) => handleClickLogin(e.target.value)} />
+        
+        <div className='form'>
+                            <input type={isShown ? "text" : "password"} className='input' value={password} placeholder="Введите пароль" onFocus={handleFocus}
+        onBlur={handleBlur} onChange={(e) => handleClickPassword(e.target.value)} />
+        <img onClick={() => setIsShown(!isShown)} src={isShown ? hide : show}/>
+        </div>
+
+        
+                            <div onClick={(e) => handleLoginBtn(e)} className='enter-button'><p>Войти</p></div>
+                            <h4 className='error'>{err}</h4>
+                        </div>
                     </div>
                 </div>
             </div>
