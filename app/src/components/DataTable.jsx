813088bbb319c9e13a17bd76dac306583fd41a93
vdmkkk <soloviov.vad@gmail.com>
2024-03-12 21:28:29 +0300

    no comment

--- app/src/components/DataTable.jsx
+++ app/src/components/DataTable.jsx
@@ -6,19 +6,34 @@ import grayPlus from "../assets/icons/grayPlus.svg"
 import { useSearchParams } from 'react-router-dom';
 
 function DataTable({ data }) {
-  console.log('111', data);
-  const [prices, setPrices] = useState(data.map((item) => {return item["price"]}));
+  // console.log('111', data);
+  const [dataa, setData] = useState([]);
+  // const [prices, setPrices] = useState(data.map((item) => {return item["price"]}));
   // console.log('hell', data.map((item) => {return item["price"]}))
-  useEffect(() => {setPrices(data.map((item) => {return item["price"]}))}, [data])
+  // useEffect(() => {
+  //   setPrices(data.map((item) => {return item["price"]}));
+  //   console.log("zaman loh");
+  // }, [data]);
+
+  useEffect(() => {setData(data)}, [data]);
+  console.log("huy", dataa)
+
+  function changePrice(key, value) {
+    var newData = [...dataa];
+    newData[key]['price'] = parseInt(value);
+    setData(newData);
+  }
+
   return (
     <div>
-        {data.map((item, key) => (
+      {/* <h1>{data[0]["price"]}</h1> */}
+        {dataa.map((item, key) => (
           <div className='raw'>
-            {/* <h1>{}</h1> */}
+            <h1>{dataa[key]["price"]}</h1>
               <p>{item["microcategory_id"]}</p>
               <p>{item["region_id"]}</p>
 
-              <div className='input-div'><input value={prices[key]} className='data-input'></input></div>
+              <div className='input-div'><input value={item["price"]} onChange={e => changePrice(key, e.target.value)} className='data-input'></input></div>
               
               <div className='empty-div'></div>
               <div className='icons-div'>
