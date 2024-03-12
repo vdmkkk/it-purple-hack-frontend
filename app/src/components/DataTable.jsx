335c43a3dbadc7dc4616e80efab1594bcc6a5854
vdmkkk <soloviov.vad@gmail.com>
2024-03-12 20:47:57 +0300

    no comment

--- app/src/components/DataTable.jsx
+++ app/src/components/DataTable.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../styles/DataTable.css"
 
 import redCross from "../assets/icons/redCross.svg"
 import grayPlus from "../assets/icons/grayPlus.svg"
+import { useSearchParams } from 'react-router-dom';
 
 function DataTable({ data }) {
+  console.log('111', data);
+  const [prices, setPrices] = useState(data.map((item) => {return item["price"]}));
+  // console.log('hell', data.map((item) => {return item["price"]}))
+  useEffect(() => {setPrices(data.map((item) => {return item["price"]}))}, [data])
   return (
     <div>
-        {data.map((item) => (
+        {data.map((item, key) => (
           <div className='raw'>
+            {/* <h1>{}</h1> */}
               <p>{item["microcategory_id"]}</p>
               <p>{item["region_id"]}</p>
 
-              <div className='input-div'><input defaultValue={item["price"]} className='data-input'></input></div>
+              <div className='input-div'><input value={prices[key]} className='data-input'></input></div>
               
               <div className='empty-div'></div>
               <div className='icons-div'>
