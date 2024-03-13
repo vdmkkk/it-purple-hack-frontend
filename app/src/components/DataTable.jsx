efec44bdd72dd280a01911168d513abbb76e17e9
vdmkkk <soloviov.vad@gmail.com>
2024-03-13 20:54:26 +0300

    main logic on files

--- app/src/components/DataTable.jsx
+++ app/src/components/DataTable.jsx
@@ -5,40 +5,89 @@ import redCross from "../assets/icons/redCross.svg"
 import grayPlus from "../assets/icons/grayPlus.svg"
 import { useSearchParams } from 'react-router-dom';
 
-function DataTable({ data }) {
-  // console.log('111', data);
+function DataTable({ data, changes, setChanges, deleted, setDeleted, added, setAdded, page }) {
   const [dataa, setData] = useState([]);
-  // const [prices, setPrices] = useState(data.map((item) => {return item["price"]}));
-  // console.log('hell', data.map((item) => {return item["price"]}))
-  // useEffect(() => {
-  //   setPrices(data.map((item) => {return item["price"]}));
-  //   console.log("zaman loh");
-  // }, [data]);
 
-  useEffect(() => {setData(data)}, [data]);
-  console.log("huy", dataa)
+  useEffect(() => {
+    var newData = data;
+    for (let i = 0; i < data.length; i++){
+      for (let j = 0; j < changes.length; j++) {
+        if (changes[j]["microcategory_id"] == data[i]["microcategory_id"] && changes[j]["region_id"] == data[i]["region_id"]) newData[i]["price"] = changes[j]["newPrice"];
+      }
+    }
+    
+    setData(newData);
+  }, [data, added]);
 
   function changePrice(key, value) {
+    var flag = false;
+    var newChanges = changes;
+    for (let i = 0; i < changes.length; i++) {
+      if (data[key]["microcategory_id"] == changes[i]["microcategory_id"] && data[key]["region_id"] == changes[i]["region_id"]) {
+          newChanges[i]['newPrice'] = value;
+        flag = true;
+      }
+    }
+    if (!flag) {
+      newChanges.push({"microcategory_id": data[key]["microcategory_id"], "region_id": data[key]["region_id"], "oldPrice": data[key]["price"], 'newPrice': value});
+    }
+    newChanges = newChanges.filter(item => item["oldPrice"] != parseInt(value));
+    setChanges(newChanges);
+
     var newData = [...dataa];
-    newData[key]['price'] = parseInt(value);
+    newData[key]['price'] = value;
     setData(newData);
+    
+    // console.log(newChanges);
   }
+
+  function handleDelete(key) {
+    setDeleted(oldDeleted => [...oldDeleted, {"microcategory_id": data[key]["microcategory_id"], "region_id": data[key]["region_id"], "price": data[key]["price"]}])
+  }
+
+  function handleAddBack(key) {
+    var newDeleted = deleted;
+    newDeleted = newDeleted.filter(item => item["microcategory_id"] != data[key]["microcategory_id"] && item["region_id"] != data[key]["region_id"]);
+    setDeleted(newDeleted);
+  }
+
+  function changeAdded(key, value, meta) {
+    // console.log('bruh', key, value, meta, added)
+    var newAdded = [...added];
+    newAdded[key][meta] = value;
+    setAdded(newAdded);
+  }
+
+  function deleteAdded(key) {
+    setAdded(added.filter(item => item["id"] != key));
+  }
+
 
   return (
     <div>
       {/* <h1>{data[0]["price"]}</h1> */}
+        {added.filter(item => item["page"] == page).map((item, key) => (
+          <div className='raw-table-added' style={{"backgroundColor":"#D1E7DD"}}>
+            <div className='input-div-added'><input type='number' value={item["microcategory_id"]} onChange={e => changeAdded(key, e.target.value, 'microcategory_id')} className='data-input'></input></div>
+            <div className='input-div-added'><input type='number' value={item["region_id"]} onChange={e => changeAdded(key, e.target.value, 'region_id')} className='data-input'></input></div>
+
+            <div className='input-div new-input'><input type='number' value={item["price"]} onChange={e => changeAdded(key, e.target.value, 'price')} className='data-input'></input></div>
+            <div style={{"width": 29}}/>
+            <div className='icons-div'>
+              <img onClick={() => deleteAdded(item["id"])} src={redCross} className='crosses-img'></img>
+            </div>
+        </div>
+        ))}
         {dataa.map((item, key) => (
-          <div className='raw'>
-            <h1>{dataa[key]["price"]}</h1>
+          <div className='raw-table' style={{"backgroundColor": deleted.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? "#f7d6e6" : changes.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? "#fff3cd"  : ""}}>
               <p>{item["microcategory_id"]}</p>
               <p>{item["region_id"]}</p>
 
-              <div className='input-div'><input value={item["price"]} onChange={e => changePrice(key, e.target.value)} className='data-input'></input></div>
-              
-              <div className='empty-div'></div>
+              <div className='input-div'><input type='number' value={item["price"]} onChange={e => deleted.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? () => {} : changePrice(key, e.target.value)} className='data-input'></input></div>
+              {changes.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? <img className='crosses-img' src={grayPlus}/> : <div style={{"width": 29}}/>}
+              {/* <div className='empty-div'></div> */}
               <div className='icons-div'>
-                <img src={grayPlus} className='crosses-img'></img>
-                <img src={redCross} className='crosses-img'></img>
+                <img onClick={() => deleted.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? handleAddBack(key) : handleDelete(key)} src={deleted.filter((item1) => item["microcategory_id"] == item1["microcategory_id"] && item["region_id"] == item1["region_id"]).length >= 1 ? grayPlus : redCross} className='crosses-img'></img>
               </div>
           </div>
         ))}
